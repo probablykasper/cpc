3c0942026f4f1fd70bca892f361cebe135644dc6
Kasper <kasperkh.kh@gmail.com>
2025-05-30 10:00:56 +0200

    Fix build

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,6 +5,9 @@ import wasm from 'vite-plugin-wasm';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit(), wasm()],
+	build: {
+		target: ['es2022', 'edge89', 'firefox89', 'chrome89', 'safari15']
+	},
 	server: {
 		fs: {
 			allow: ['../pkg']
