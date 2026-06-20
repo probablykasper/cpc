f802ff52f89a64d139bb8075f6e1ddb1560f3e68
Kasper <kasperkh.kh@gmail.com>
2026-06-20 02:38:25 +0200

    Format

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-vercel";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter() },
 };
 
 export default config;
